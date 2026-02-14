399915c89f4ba280cd0f5ea264ce925269501a5d
Bruce Carvalho <bruce.carvalho@wundermanthompson.com>
2026-02-14 11:07:04 +0000

    docs: comment out map component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
                     ))}
                 </section>
 
-                <Location />
+                {/* <Location /> */}
                 <Footer />
             </main>
         </div>
